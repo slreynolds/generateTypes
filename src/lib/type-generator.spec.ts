19de9b26002f3d1beedb16ffaec4e34116608f5d
Steven <reynolds.tud@gmail.com>
2020-05-01 12:47:03 +0200

    fixes on tests

--- src/lib/type-generator.spec.ts
+++ src/lib/type-generator.spec.ts
@@ -7,62 +7,61 @@ import 'mocha';
 describe("Basic JSON Types", () => {
   it("number", () => {
     expect(new TypeGenerator().magic('Test','{"a": 1}')).to.equal(
-      'type Test = {\n  a: number\n}');
+      'type Test = {\n  a: number\n}\n');
   });
   it("string", () => {
     expect(new TypeGenerator().magic('Test','{"a": "b"}')).to.equal(
-      'type Test = {\n  a: string\n}');
+      'type Test = {\n  a: string\n}\n');
   });
   it("boolean", () => {
     expect(new TypeGenerator().magic('Test','{"a": true}')).to.equal(
-      'type Test = {\n  a: boolean\n}');
+      'type Test = {\n  a: boolean\n}\n');
   });
   it("null", () => {
     expect(new TypeGenerator().magic('Test','{"a": null}')).to.equal(
-      'type Test = {\n  a: null\n}');
+      'type Test = {\n  a: null\n}\n');
   });
-  it("null2", () => {
-    expect(new TypeGenerator().magic('Test','{"a": undefined}')).to.equal(
-      'type Test = {\n  a: null\n}');
-  });
+
 });
 
 describe("Arrays", () => {
-  it("String array", () => {
+  it("string array", () => {
     expect(new TypeGenerator().magic('Test',
       '{"custom_audiences": ["a","b","c"]}')).to.equal(
-      'type Test = {\n  custom_audiences: string[]\n}');
+      'type Test = {\n  custom_audiences: string[]\n}\n');
   });
 });
 
 describe("Objects", () => {
-  it("object", () => {
+  it("object with string array", () => {
     expect(new TypeGenerator().magic('Test',
       '{"custom_audiences": ["a","b","c"]}')).to.equal(
-      'type Test = {\n  custom_audiences: string[]\n}');
+      'type Test = {\n  custom_audiences: string[]\n}\n');
   });
 });
 
 describe("Edge Cases", () => {
-  it("just a empty object", () => {
+  it("empty name", () => {
+    expect(new TypeGenerator().magic('','{"a": 1}')).to.equal(
+      'type Thing = {\n  a: number\n}\n');
+  });
+  it("empty object", () => {
     expect(new TypeGenerator().magic('Test','{}')).to.equal(
-      'type Test = {\n}');
+      'type Test = {\n}\n');
   });
-  it("just a empty array", () => {
+  it("empty array", () => {
     expect(new TypeGenerator().magic('Test','[]')).to.equal(
-      'type Test = [\n]');
+      'type Test = [\n]\n');
   });
-
 });
 
 describe("Faulty stuff", () => {
-  it("just a empty object", () => {
-    expect(new TypeGenerator().magic('Test','{}')).to.equal(
-      'type Test = {\n}');
+  it("Unexpected Token", () => {
+    expect(new TypeGenerator().magic('Test','abc')).to.throw(
+      'SyntaxError: Unexpected token u in JSON at position 0');
   });
-  it("just a empty array", () => {
-    expect(new TypeGenerator().magic('Test','[]')).to.equal(
-      'type Test = [\n]');
+  it("empty", () => {
+    expect(new TypeGenerator().magic('Test','')).to.throw(
+      'SyntaxError: Unexpected end of JSON input');
   });
-
 });
